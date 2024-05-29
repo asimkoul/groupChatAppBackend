25bd18eee3f9e244f765f1724c83beefe5d9b591
asimkoul <ahmadasim080gmail.com>
2024-05-29 23:04:54 +0530

    loginFrontend

---
+++ public/Login/login.js
@@ -0,0 +1,18 @@
+async function login(event) {
+    try {
+        event.preventDefault()
+        const loginDetails={
+            email:event.target.email.value,
+            password:event.target.password.value
+        }    
+        const response=await axios.post("http://localhost:3000/user/login",loginDetails)
+        if (response.status===200) {
+            alert(response.data.message)
+            console.log(response.data);
+            localStorage.setItem('token',response.data.token)
+             window.location.href="/"
+        } 
+    } catch (error) {
+        console.log(error)
+    }
+}
