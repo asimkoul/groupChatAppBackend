e0761701a1d70a5f4a3b60def0d01525c9577f59
asimkoul <ahmadasim080gmail.com>
2024-06-04 19:15:52 +0530

    deploymentready1

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express=require('express')
+const dotenv=require('dotenv')
+dotenv.config()
 const cors=require('cors')
 const bodyParser=require('body-parser')
 const sequelize=require('./util/database')
